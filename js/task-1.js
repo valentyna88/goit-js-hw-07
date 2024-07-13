91feabd0562ac14dcfb65ccfcc0f8041c3ae3eec
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-13 19:46:19 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ console.log(`Numbers of categories: ${listItems.length}`);
 
 listItems.forEach(item => {
   const categoryTitle = item.querySelector('h2').textContent;
-  const elementsAmount = item.querySelectorAll('ul li').length;
+  const elementsAmount = item.querySelectorAll('ul>li').length;
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${elementsAmount}`);
 })
