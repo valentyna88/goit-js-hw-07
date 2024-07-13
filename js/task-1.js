549d73182ae4930204f910089c9480bba612247d
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-13 19:38:03 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,10 @@
-const listWithId = document.querySelector('#categories');
+const listItems = document.querySelectorAll('.item');
+
+console.log(`Numbers of categories: ${listItems.length}`);
+
+listItems.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const elementsAmount = item.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsAmount}`);
+})
