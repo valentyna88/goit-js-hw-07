721b46827efad08d445d781c2f68f64aedd4f2f6
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-15 00:09:55 +0300

    Update task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const nameInput = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+nameInput.addEventListener("input", () => {
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
