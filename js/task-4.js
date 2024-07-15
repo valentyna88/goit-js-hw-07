136d66285b80af39be7211a868681df1d693f326
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-15 10:53:52 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
-const button =  document.querySelector()
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailValue = form.elements.email.value.trim();
+  const passwordValue = form.elements.password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue
+  };
+
+  console.log(formData);
+
+  form.reset();
+});
+
+
