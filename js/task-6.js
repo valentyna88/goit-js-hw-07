6330b51176990a3085cf7d08e6e04f1925fd4665
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-16 09:45:56 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxes = document.getElementById('boxes');
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  boxes.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.classList.add('box');
+    boxes.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
